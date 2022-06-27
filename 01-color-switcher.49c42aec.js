04d840a2d08b61d71d0eeec3144ee3bb8ed5cd44
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-27 20:07:11 +0000

    Deploying to gh-pages from @ Mykola28112002/goit-js-hw-09@2210534200d989063a7566f2ca7fb62f4350e51e 🚀

--- 01-color-switcher.49c42aec.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");e.addEventListener("click",(()=>{e.setAttribute("disabled",!0),timerId=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3)})),r.addEventListener("click",(()=>{e.removeAttribute("disabled"),clearInterval(timerId)}));
-//# sourceMappingURL=01-color-switcher.49c42aec.js.map
