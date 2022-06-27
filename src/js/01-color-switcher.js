03f897106607e389d89d84375217a85a10d03ba7
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-27 21:23:58 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,2 @@
-const bodyEl = document.querySelector('body');
-const btnStart = document.querySelector('[data-start]');
-const btnStop = document.querySelector('[data-stop]');
 
-
-btnStart.addEventListener("click", () => {
-    btnStart.setAttribute('disabled', true);
-    timerId = setInterval(() => {
-    const hexColor = getRandomHexColor();
-    bodyEl.style.backgroundColor = hexColor;
-  }, 1000);
-});
-
-btnStop.addEventListener("click", () => {
-    btnStart.removeAttribute('disabled');
-  clearInterval(timerId);
-});
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 
