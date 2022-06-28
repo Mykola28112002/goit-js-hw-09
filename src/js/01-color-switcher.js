6cf639fd8257c8727a95a073105467a47001fef0
Mykola28112002 <95100219+Mykola28112002@users.noreply.github.com>
2022-06-28 10:30:18 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,14 +10,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 function startColor() {
-    btnStart.setAttribute('disabled', true);
-    timerId = setInterval(() => {
+  btnStart.setAttribute('disabled', true);
+  timerId = setInterval(() => {
     const hexColor = getRandomHexColor();
     bodyEl.style.backgroundColor = hexColor;
   }, 1000);
 }
+
 function stopColor() {
-    btnStart.removeAttribute('disabled');
-  clearInterval(timerId);
+  btnStart.removeAttribute('disabled');
+  const id = timerId;
+  clearInterval(id);
 }
 
